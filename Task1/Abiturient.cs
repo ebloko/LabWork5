97fca50197b0e7b2b9ae9595dbf282ccf47c8b82
une pomme <AstafevK@yandex.ru>
2022-02-12 18:16:59 +0300

    LabWork5

---
+++ Task1/Abiturient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Abiturient
+    {
+        private string FullName;
+        private int Class;
+        private double AVG;
+
+        public string FullName1 { get => FullName; set => FullName = value; }
+        public int Class1 
+        { 
+            get => Class;
+            set
+            {
+                if (value == 11)
+                    Class = value;
+            } 
+        }
+        public double AVG1 { get => AVG; set => AVG = value; }
+
+        public Abiturient(string FullName, int Class, double Avg)
+        {
+            this.FullName1 = FullName;
+            this.Class1 = Class;
+            this.AVG1 = AVG1;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"ФИО: {FullName1} Класс: {Class1} Среднйи балл: {AVG1}");
+        }
+
+        internal Abiturient(string FullName, int Class, int AVG)
+        {
+            Console.WriteLine($" {FullName} {Class} {AVG}");
+        }
+        public void Print()
+        {
+            Console.WriteLine($" {FullName} {Class1 > 9}");
+
+        }
+
+
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+   
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Abiturient Name = new Abiturient("Петр Петров Петров", 9, 4.5);
+            Abiturient Name1 = new Abiturient("Иван Иванов Иванович", 11, 5);
+            Abiturient Name2 = new Abiturient("Сергей Иванов Янович", 9, 5);
+            Abiturient Name3 = new Abiturient("Владимир Иванов Янович", 9, 5);
+            Abiturient Name4 = new Abiturient("Ян Иванов Янович", 11, 5);
+            Abiturient Name5 = new Abiturient("Ян Иванов Янович", 11, 5);
+            Abiturient Name6 = new Abiturient("Ян Иванов Янович", 11, 5);
+
+            Abiturient.Output();
+            Name1.Print();
+            
+        }
+
+    }
+}
